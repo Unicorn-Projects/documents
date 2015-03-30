2d144edfb2a322d0be5a1ee96afc6f4098096380
Xingbo Wang <shawn.xingbo.wang@gmail.com>
2015-03-29 18:16:51 -0700

    add rest api test

---
+++ rest_api_test/update_cuisine_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import os
+
+# register customer
+	# there is no api to register customer right now, so we need manually create one
+
+# customer login, get access credential
+	# with customer login, password, we can get an access token 
+	# with the access token and customer principal, we can generate auth header
+# Create restaurant
+	# there is no api to create restaurant right now, so we need manually create one
+# Create cuisine
+	# there is no api to create cuisine right now, so we need manually create one
+# Update cuisine info
+	# with the auth header and the cuisine info we want to update,
